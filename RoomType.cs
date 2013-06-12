a749dddc9548cbcbafb3b28f01628a2972dfffc8
Sandipan Das <sandipandas@mail.com>
2013-06-12 19:44:07 +0530

    Create RoomType.cs

---
+++ RoomType.cs
@@ -0,0 +1,17 @@
+
+namespace Cognizant.HRS.BusinessEntities
+{
+  /// <summary>
+	/// Representation of a RoomType as an entity
+	/// </summary>
+	public class RoomType
+	{
+		#region "Public Properties"
+
+		public int RoomTypeId { get; set; }
+		public string RoomType { get; set; }
+		public string RoomDescription { get; set; }
+
+		#endregion
+	}
+}
