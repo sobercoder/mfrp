6101550481c81e43dad27b7253e0a2e5a8b25bd8
Sandipan Das <sandipandas@mail.com>
2013-06-12 19:43:44 +0530

    Create Bank.cs

---
+++ Bank.cs
@@ -0,0 +1,16 @@
+
+namespace Cognizant.HRS.BusinessEntities
+{
+  /// <summary>
+	/// Representation of a Bank as an entity
+	/// </summary>
+	public class Bank
+	{
+		#region "Public Properties"
+
+		public string BankId { get; set; }
+		public string BankName { get; set; }
+
+		#endregion
+	}
+}
